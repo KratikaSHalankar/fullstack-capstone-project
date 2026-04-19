bbf0c53893b4154a20b0f3c5a880093e7e32026f
Divya S Nayak <divyasnayak05@gmail.com>
2026-04-19 20:41:59 +0530

    Create RegisterPage.js

---
+++ giftlink-frontend/src/pages/RegisterPage.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+const RegisterPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:5000/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password
+        })
+      });
+
+      const data = await response.json();
+      console.log("Register Success:", data);
+      alert(data.message);
+
+    } catch (error) {
+      console.error("Error registering user:", error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+
+      <form onSubmit={handleRegister}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterPage;
